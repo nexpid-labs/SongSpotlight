2998116d25c1014de221d481525aec4e6b3c7d16
nexpid <me@nexpid.xyz>
2026-03-04 13:37:43 +0100

    refactor: add refresh tokens

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,31 +2,66 @@ import { jwtVerify, SignJWT } from "jose";
 
 export interface TokenPayload {
 	userId: string;
+	exp: number;
 }
 
+// used for breaking changes related to auth
+const issuer = "song-spotlight:1";
 const alg = "HS256";
-const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-const expirationTime = "1y";
 
-export async function getUser(token?: string): Promise<TokenPayload | null> {
-	if (!token) return null;
+const accessSecret = new TextEncoder().encode(process.env.JWT_SECRET);
+const accessExpiry = "12h";
 
+const refreshSecret = new TextEncoder().encode(process.env.JWT_REFRESH_SECRET);
+const refreshExpiry = "2y";
+
+type TokenType = "access" | "refresh";
+
+async function parseUser(token: string, tokenType: TokenType): Promise<TokenPayload | null> {
+	const isRefresh = tokenType === "refresh";
 	try {
-		const verified = await jwtVerify<TokenPayload>(token, secret, {
-			algorithms: [alg],
-		});
+		const verified = await jwtVerify<TokenPayload>(
+			token,
+			isRefresh ? refreshSecret : accessSecret,
+			{
+				algorithms: [alg],
+				maxTokenAge: isRefresh ? refreshExpiry : accessExpiry,
+				issuer,
+			},
+		);
 		return verified.payload;
 	} catch {
 		return null;
 	}
 }
 
-export async function createToken(userId: string): Promise<string> {
+export async function getUser(token?: string, tokenType: TokenType = "access") {
+	const user = token && await parseUser(token, tokenType);
+	return user && user.exp > Date.now() / 1000 ? user : null;
+}
+export async function isValid(token?: string, tokenType: TokenType = "access") {
+	const user = token && await parseUser(token, tokenType);
+	return !!user;
+}
+
+export async function createAccessToken(userId: string) {
 	return await new SignJWT({
 		userId,
 	})
 		.setProtectedHeader({ alg })
 		.setIssuedAt()
-		.setExpirationTime(expirationTime)
-		.sign(secret);
+		.setIssuer(issuer)
+		.setExpirationTime(accessExpiry)
+		.sign(accessSecret);
+}
+
+export async function createRefreshToken(userId: string) {
+	return await new SignJWT({
+		userId,
+	})
+		.setProtectedHeader({ alg })
+		.setIssuedAt()
+		.setIssuer(issuer)
+		.setExpirationTime(refreshExpiry)
+		.sign(refreshSecret);
 }
